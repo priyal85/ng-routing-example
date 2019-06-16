5673b35f644d52914fd660e8de75d2d66b14e61a
Priyal Samaratunga <priyal.samaratunga@cambio.lk>
2019-06-16 23:06:15 +0530

    Initial commit with routing examples

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'routing-example';
+  routs = [{ linkName: 'Home', url: 'home' }, { linkName: 'Settings', url: 'settings' }];
+  homeRoute = 'home';
+  settingsRoute = 'settings';
 }
